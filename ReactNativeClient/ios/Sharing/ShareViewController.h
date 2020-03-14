0c5e19f746de45dd515adab2126c5e836feee65c
Aaryan Kapur <ak6392@bennett.edu.in>
2020-03-14 21:57:43 +0530

    ios-edit-sharing

---
+++ ReactNativeClient/ios/Sharing/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  Sharing
+//
+//  Created by Aaryan Kapur on 14/03/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
